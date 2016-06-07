4904189d291a681dd6cd7c3484b8932e0735e06f
Elias Strizower <ebs021@bucknell.edu>
2016-06-07 15:48:23 -0400

    began work on header for transport class

---
+++ Transport.hpp
@@ -0,0 +1,20 @@
+#ifndef __TRANSPORT__
+#define __TRANSPORT__
+#include <string>
+
+class Transport {
+
+
+
+
+private:
+
+public:
+  Transport()
+  ~Transport()
+
+void write(std::string msg)
+
+};
+
+#endif
