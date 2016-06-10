c95dcab98a0c5a36718b55808126d1f2057010b5
Elias Strizower <ebs021@bucknell.edu>
2016-06-10 15:21:57 -0400

    Added boost test cases and struct

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,13 +1,55 @@
 #include "AxFastLog.hpp"
-#include <iostream>
+#include <boost/test/unit_test.hpp>
+#define BOOST_TEST_MODULE AxFastLog test
 
 
+struct axFastLogVars{
+std:: string axFilePath = "../Desktop/axTest.txt"
+std:: string  transFilePath= "../Desktop/transTest.txt"
 std::string testString = "TEST";
-
 Severity testSev = Severity::INFO;
+std:: string expectedString = testString;
+std:: string calculatedString;
+std:: string  transCalcStr;
+std:: string transExpectedStr = testString;
 
-AxFastLog::testAxFastLog(LogEnums:: FILE, "STRING");
+};
 
-testAxFastLog::TEST_LOG(testString, testSev){
-  testAxFastLog.log(testString, testSev);
+
+BOOST_FIXTURE_TEST_SUITE(axFastLogSuite, axFastLogVars);
+
+BOOST_AUTO_TEST_CASE(axFastLogTest){
+  AxFastLog ax(LogEnums:: FILE, axFilePath);
+  ax.log(testString, testSev);
+
+  //read from file and write to calculatedString
+  ifstream myReadFile;
+  myReadFile.open(axFilePath);
+  if(myReadFile.is_open()){
+    while (!myReadFile.eof()) {
+      myReadFile >> std:: calculatedString;
+    }
+  }
+  myReadFile.close();
+  BOOST_CHECK_MESSAGE(std::strncmp(expectedString,calculatedString) == 0, "ERROR: Expected string not equal to calculated string" );
+
 }
+
+
+BOOST_AUTO_TEST_CASE(fileTransportTester){
+  FileTransport fileTrans(transFilePath);
+  fileTrans.write(testString);
+
+  ifstream myReadfile;
+  myReadFile.open(transFilePath);
+  if(myReadFile.is_open()){
+    while (!myReadFile.eof()) {
+      myReadFile >> std:: transCalcStr;
+    }
+  }
+  myReadFile.close();
+  BOOST_CHECK_MESSAGE(std::strncmp(transCalcStr, transExpectedStr) == 0, "ERROR: Expected string not equal to calculated string" );
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
