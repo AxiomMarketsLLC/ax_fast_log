434069a8f137dad5c2e79dcd36e5cb1811d0649e
Tyler McDaniel <bmcdanie@unca.edu>
2016-06-22 16:58:47 -0400

    Basic performance test by cycles added

--- perf/perf.cpp
+++ perf/perf.cpp
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string>
+#include <iostream>
+#include "../AxFastLog.hpp"
+#define AX_THREAD_TIMEOUT_MS 1
 //from intel devel guide: indirect: http://stackoverflow.com/questions/459691/best-timing-method-in-ci
 
 inline uint64_t rdtsc() {
@@ -14,13 +18,52 @@ inline uint64_t rdtsc() {
     return (uint64_t)hi << 32 | lo;
 }
 
-int main()
-{
+
+
+void test_printf_cycles(std::string testString) {
+    unsigned long long x;
+    unsigned long long y;
+    const char* testChars = testString.c_str();
+    x = rdtsc();
+    printf(testChars);
+    y = rdtsc();
+    printf("\n");
+
+    printf("PERF: CYCLES: printf: %lld\n",y-x);
+}
+
+void test_cout_cycles(std::string testString) {
+
     unsigned long long x;
     unsigned long long y;
     x = rdtsc();
-    printf("%lld\n",x);
+    std::cout << testString << std::endl;
     y = rdtsc();
-    printf("%lld\n",y);
-    printf("PERF: CYCLES: printf: %lld\n",y-x);
+    printf("PERF: CYCLES: cout: %lld\n",y-x);
+}
+
+void test_axlog_cycles(std::string testString) {
+    
+    unsigned long long x;
+    unsigned long long y;
+    AxFastLog ax(LogEnums::FILE, "test.txt", AX_THREAD_TIMEOUT_MS);
+    x = rdtsc();
+    ax.log(testString, LogEnums::INFO);
+    y = rdtsc();
+    printf("PERF: CYCLES: ax_log (file): %lld\n",y-x);
+
+}
+
+int main()
+{
+
+
+    std::string testString = "TESTING 1, 2, 3"; 
+    test_printf_cycles(testString);
+    test_cout_cycles(testString);
+    test_axlog_cycles(testString);
+
+    
+  
+ 
 }
