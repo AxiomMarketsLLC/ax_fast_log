214e4dcadb82b6801049d5e3725f461601d1d020
bmcdanie <bmcdanie@unca.edu>
2016-06-23 22:13:40 -0400

    Linker error resolved, working script

--- test_travis.sh
+++ test_travis.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 mkdir lib
 make clean
 make test
 cd test
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(usr/bin/pwd)/../lib/"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(usr/bin/pwd)/../libs/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(PWD)/../lib/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(PWD)/../libs/"
 ./ax_fast_log_test
