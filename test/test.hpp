82fc2e7445764c019ea00a9ac064dfd712d39a94
malrazd <mallikarazdan@yahoo.co.in>
2016-06-29 16:21:48 -0400

    Fixed NotTrivial class

--- test/test.hpp
+++ test/test.hpp
@@ -1,11 +1,13 @@
 #ifndef __NOTTRIVIAL__
 #define __NOTTRIVIAL__
-
+#include <stdio.h>
 
 class NotTrivial {
     public:
      ~NotTrivial();
 };
-NotTrivial::~NotTrivial() {}
+NotTrivial::~NotTrivial() {
+	fprintf(stdout,"");
+}
 
 #endif
